4abc862bbeee1573a0317adf84a9d29ee74c06d0
jasson99 <jasmine.baral@hotmail.com>
2020-05-07 12:22:08 +0545

    apiAuth

--- tests/acceptance/features/bootstrap/Provisioning.php
+++ tests/acceptance/features/bootstrap/Provisioning.php
@@ -1004,6 +1004,7 @@ trait Provisioning {
 	 * @return void
 	 */
 	public function adminSendsUserCreationRequestUsingTheProvisioningApi($user, $password) {
+		$user = $this->getActualUsername($user);
 		$password = $this->getActualPassword($password);
 		$bodyTable = new TableNode([['userid', $user], ['password', $password]]);
 		$this->ocsContext->userSendsHTTPMethodToOcsApiEndpointWithBody(
@@ -1823,6 +1824,7 @@ trait Provisioning {
 	 * @throws \Exception
 	 */
 	public function userHasBeenDeleted($user) {
+		$user = $this->getActualUsername($user);
 		if ($this->userExists($user)) {
 			if ($this->isTestingWithLdap() && \in_array($user, $this->ldapCreatedUsers)) {
 				$this->deleteLdapUser($user);
